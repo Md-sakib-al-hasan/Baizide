44634ab177dfa8e6ec16059bc2004c0678f36b37
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-02-26 20:32:24 +0600

    sk

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,17 @@
+
+
+const Banner = () => {
+  return (
+    <div>
+        <div className="w-full relative">
+            <img src="" alt="banner" />
+            <div className="absolute inset-0 ">
+
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Banner
